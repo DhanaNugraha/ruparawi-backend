b90703dea66b01eecb2d637f9ebdacea2035a359
DhanaNugraha <dhananugraha1511@gmail.com>
2025-04-25 00:53:30 +0700

    feat: creating admin role
    Made a separate table for admin for security and an admin log table. Made wrapper for super admin and regular admin. Configured router for admin for later use.

--- auth/auth.py
+++ auth/auth.py
@@ -1,16 +1,44 @@
 from functools import wraps
-from flask import jsonify
 from flask_jwt_extended import get_jwt_identity
-from repo.user import user_by_id_repo
+from repo.user import admin_by_id_repo
 
 
-def admin_required(fn):
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        user_id = get_jwt_identity()
-        user = user_by_id_repo(user_id)
-        if not user or not user.is_admin:
-            return jsonify({"error": "Admin access required"}), 403
-        return fn(*args, **kwargs)
+def super_admin_required():
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            user_id = get_jwt_identity()
+
+            admin = admin_by_id_repo(user_id)
+
+            if not admin:
+                return {"error": "Admin privileges required"}, 403
+            
+            elif admin.access_level != "super":
+                return {"error": "Super admin required"}, 403
+
+            else:
+                return fn(*args, **kwargs)
+
+        return decorator
+
+    return wrapper
+
+
+def admin_required():
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            user_id = get_jwt_identity()
+
+            admin = admin_by_id_repo(user_id)
+
+            if not admin:
+                return {"error": "Admin privileges required"}, 403
+
+            else:
+                return fn(*args, **kwargs)
+
+        return decorator
 
     return wrapper
--- models/user.py
+++ models/user.py
@@ -15,7 +15,6 @@ class User(db.Model, BaseModel):
     profile_image_url = db.Column(db.String(255))
     bio = db.Column(db.Text)
     is_vendor = db.Column(db.Boolean, default=False)
-    is_admin = db.Column(db.Boolean, default=False)
     last_login = db.Column(db.DateTime)
 
     # Relationships
@@ -26,6 +25,7 @@ class User(db.Model, BaseModel):
     reviews_written = db.relationship("ProductReview", backref="reviewer", lazy=True)
     # uselist false for one to one. lazy joined to get cart with user.cart
     cart = db.relationship("ShoppingCart", backref="user",  uselist=False, lazy="joined" )
+    # admin = db.relationship("AdminUser", backref="user", uselist=False, lazy="joined")
 
     @property
     def password(self):
@@ -69,3 +69,31 @@ class UserPaymentMethod(db.Model, BaseModel):
     account_number = db.Column(db.String(100), nullable=False)
     expiry_date = db.Column(db.Date)
     is_default = db.Column(db.Boolean, default=False)
+
+
+# admin will be manually added in the database to be safe
+# Separate admin table for safety when querying user
+class AdminUser(db.Model):
+    __tablename__ = 'admin_users'  
+
+    # user_id is the primary key to ensure unique user id to admin
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    access_level = db.Column(db.String(20))  #  super, admin
+
+    # Relationships to separate user admin and granter id
+    user = db.relationship(
+        "User",
+        foreign_keys=[user_id],
+        backref=db.backref(
+            "admin_profile",
+            uselist=False,  # One-to-one relationship
+        ),
+    )
+
+# logs of admin actions
+class AdminLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    admin_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    action = db.Column(db.String(50))  # Ex: "user_deleted"
+    target_id = db.Column(db.Integer)  # Affected user_id
+    timestamp = db.Column(db.DateTime, default=time.now())
