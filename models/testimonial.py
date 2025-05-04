23a2d90b5ab66aa1cf056f1cd3917133835e37b8
anggreinipra <anggreinipra@gmail.com>
2025-05-04 23:28:52 +0700

    fix/testimonial remove admin_id

--- models/testimonial.py
+++ models/testimonial.py
@@ -5,9 +5,7 @@ class VendorTestimonial(db.Model, BaseModel):
     __tablename__ = 'vendor_testimonials'
     
     vendor_id = db.Column(db.Integer, db.ForeignKey("vendor_profiles.user_id"), nullable=False)
-    admin_id = db.Column(db.Integer, db.ForeignKey("admin_users.user_id"), nullable=False)
     message = db.Column(db.Text, nullable=False)
 
     # Relationships
     vendor = db.relationship("VendorProfile", backref=db.backref("testimonials", lazy=True))
-    admin = db.relationship("AdminUser", backref=db.backref("testimonials", lazy=True))
