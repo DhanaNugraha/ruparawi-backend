671dad31a245773941a850e38cab8a46647a254e
anggreinipra <anggreinipra@gmail.com>
2025-05-05 16:50:47 +0700

    fix/article img_url len

--- models/article.py
+++ models/article.py
@@ -6,7 +6,7 @@ class Article(db.Model, BaseModel):
 
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    image_url = db.Column(db.String(500), nullable=True)
+    image_url = db.Column(db.String(2048), nullable=True)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     
     author = db.relationship("User", backref="articles")
